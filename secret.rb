14c22287f8eb44746031541806856bec59ecde07
Jacek Materna <jacek@assembla.com>
2017-12-12 15:10:54 -0600

    secret key

---
+++ secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DB_PASSWORD = 'e5d611653ee09443f2dd000884ba4214333cdff7a7f5ce4dff9e96be064e4b5b5ff5fd0e0dbef11fdd0dc84fa1166b79c1a91a439708435b7522f6a5b0cdac3d'
